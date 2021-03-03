6c3b71b1136b1f97d36e929cd5403d02bfb36bd1
homchenko <posharkow@mail.ru>
2021-03-03 18:19:06 +0300

    fedat: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arr = [];
+    const openBrackets = ['(','{','['];
+    const closedBrackets = [')','}',']'];
+    for (let letter of str) { 
+        if(openBrackets.includes(letter)){ 
+            arr.push(letter);
+        }else if(closedBrackets.includes(letter)){ 
+            const openPair = openBrackets[closedBrackets.indexOf(letter)]; 
+            if(arr[arr.length - 1] === openPair){ 
+                arr.splice(-1,1);
+            }else{ 
+                arr.push(letter);
+                break;
+            }
+        }
+    }
+    return (arr.length === 0);
 }
